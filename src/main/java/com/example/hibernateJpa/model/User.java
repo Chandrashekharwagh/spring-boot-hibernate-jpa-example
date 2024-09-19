559350438f65e38d64f4d83bf2d1788c6678350c
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-19 18:59:36 +0530

    commits

---
+++ src/main/java/com/example/hibernateJpa/model/User.java
@@ -0,0 +1,25 @@
+package com.example.hibernateJpa.model;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Data // Lombok annotation to generate getters, setters, toString, etc.
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "first_name", nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
+
+    @Column(name = "email", nullable = false, unique = true)
+    private String email;
+}
