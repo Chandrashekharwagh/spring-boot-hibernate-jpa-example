26a8726c20325d166e0f16c730fdc7f1041f64d9
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-20 12:13:31 +0530

    commits

--- src/main/java/com/example/hibernateJpa/service/UserService.java
+++ src/main/java/com/example/hibernateJpa/service/UserService.java
@@ -2,6 +2,8 @@ package com.example.hibernateJpa.service;
 
 import com.example.hibernateJpa.model.User;
 import com.example.hibernateJpa.repository.UserRepository;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -9,19 +11,23 @@ import java.util.List;
 
 @Service
 public class UserService {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserRepository userRepository;
 
     public List<User> getAllUsers() {
+        logger.info("Users Retrieved");
         return userRepository.findAll();
     }
 
     public User getUserById(Long id) {
+        logger.info("User detected by ID");
         return userRepository.findById(id).orElse(null);
     }
 
     public User createUser(User user) {
+        logger.info("User Created");
         return userRepository.save(user);
     }
 
@@ -31,12 +37,14 @@ public class UserService {
             user.setFirstName(updatedUser.getFirstName());
             user.setLastName(updatedUser.getLastName());
             user.setEmail(updatedUser.getEmail());
+            logger.info("User Updated");
             return userRepository.save(user);
         }
         return null;
     }
 
     public void deleteUser(Long id) {
+        logger.info("User deleted");
         userRepository.deleteById(id);
     }
 }
