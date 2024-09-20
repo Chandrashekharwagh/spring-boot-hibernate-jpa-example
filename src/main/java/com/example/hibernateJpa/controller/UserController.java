ca1b25ca6be9bcc4a98760d2282565be1aa218af
Chandrashekharwagh <waghchandu2002@gmail.com>
2024-09-20 12:13:26 +0530

    commits

--- src/main/java/com/example/hibernateJpa/controller/UserController.java
+++ src/main/java/com/example/hibernateJpa/controller/UserController.java
@@ -2,6 +2,8 @@ package com.example.hibernateJpa.controller;
 
 import com.example.hibernateJpa.model.User;
 import com.example.hibernateJpa.service.UserService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -10,19 +12,22 @@ import java.util.List;
 
 
 @RestController
-@RequestMapping("/api/users")
+@RequestMapping("/userManagement/users")
 public class UserController {
+    private static final Logger logger = LoggerFactory.getLogger(UserService.class);
 
     @Autowired
     private UserService userService;
 
     @GetMapping
     public List<User> getAllUsers() {
+        logger.info("/Retrieving users from Get");
         return userService.getAllUsers();
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<User> getUserById(@PathVariable Long id) {
+        logger.info("/Retrieving users from Get by ID");
         User user = userService.getUserById(id);
         if (user != null) {
             return ResponseEntity.ok(user);
@@ -32,11 +37,13 @@ public class UserController {
 
     @PostMapping
     public User createUser(@RequestBody User user) {
+        logger.info("/Creating users from Post");
         return userService.createUser(user);
     }
 
     @PutMapping("/{id}")
     public ResponseEntity<User> updateUser(@PathVariable Long id, @RequestBody User user) {
+        logger.info("/Updating users from Put");
         User updatedUser = userService.updateUser(id, user);
         if (updatedUser != null) {
             return ResponseEntity.ok(updatedUser);
@@ -46,6 +53,7 @@ public class UserController {
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteUser(@PathVariable Long id) {
+        logger.info("/deleting users from delete");
         userService.deleteUser(id);
         return ResponseEntity.noContent().build();
     }
